cf3df2f5cc026446e68bc9d2501f0182001d9fa3
BarsDiman <05106881@rambler.ru>
2022-09-07 16:39:58 +0400

    first commit

---
+++ src/main/java/com/sergeev/spring/rest/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.sergeev.spring.rest.service;
+
+import com.sergeev.spring.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
